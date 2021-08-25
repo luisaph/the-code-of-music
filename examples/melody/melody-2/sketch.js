5b2a48916a9465935cdf9b93bd02cb5886fbfe29
Stuti mohgaonkar <stuti.mohgaonkar@gmail.com>
2021-08-25 14:25:36 -0400

    updates interactives

--- examples/melody/melody-2/sketch.js
+++ examples/melody/melody-2/sketch.js
@@ -6,9 +6,9 @@ window.registerP5Sketch((p) => {
     let previousPosition = [];
     let c;
     let rc;
-    let circles = 60;
+    let circles = 50;
     let amp = 6;
-    let divs = 2;
+    let divs = 4;
     let x, y, w, h;
     let cy,cx;
 
@@ -28,8 +28,8 @@ window.registerP5Sketch((p) => {
     let audioContext;
     let pitch;
     fft = new Tone.FFT();
-
-    let buttonStyle = `
+    let playBtn;
+    let buttonStylemp = `
     display: block;
     padding: 20px;
     background-image:url("${assetsUrl}/img/microphone.png");
@@ -41,6 +41,7 @@ window.registerP5Sketch((p) => {
   `;
 
     p.preload = () => {
+        console.log('assetsUrl',assetsUrl);
         mouth = p.loadImage(assetsUrl+"/img/mouth.png");
         ear = p.loadImage(assetsUrl+"/img/ear.png");
     }
@@ -48,7 +49,7 @@ window.registerP5Sketch((p) => {
         c = p.createCanvas(800, 300);
         mic = new Tone.UserMedia().toDestination();
         playBtn = p.createButton('');
-        playBtn.style(buttonStyle);
+        playBtn.style(buttonStylemp);
         playBtn.mouseReleased(togglePlay);
         //playBtn.position(p.width/2, p.height/2);
         
@@ -65,15 +66,15 @@ window.registerP5Sketch((p) => {
         y = p.height / 2;
         w = p.width - 2 * x;
         cy = p.height/2;
-        cx = p.width/4+20;
-        p.image(ear, cx+p.width-ear.width-190, p.height/3+10, 50, ear.height*(50)/ear.width);
+        cx = p.width/4-40;
+        p.image(ear, cx+p.width-ear.width-240, p.height/3, 80, ear.height*(80)/ear.width);
         p.push()
             p.textSize(12);
             let textString = '';
            
             for(let i=0;i<circles;i++) {
                 //draw circle
-                let r = (20 + 8*i + amp*(p.sin((p.frameCount)*(2*p.PI*freq*0.003) + (p.PI/4.5*i))))
+                let r = (20 + 9*i + amp*(p.sin((p.frameCount)*(2*p.PI*freq*0.002) + (p.PI/4.5*i))))
                 p.push()
                     p.noFill();
                     p.strokeWeight(0.6);
@@ -87,8 +88,8 @@ window.registerP5Sketch((p) => {
                     phaseDiff = Math.abs(freq-prevFreq)*2*PI*frameCount
                 }*/
                 let div = divs
-                if(i < 8) {
-                    div = (9-i)*divs
+                if(i < 6) {
+                    div = (7-i)*divs
                 }
                 for(let j=0;j<p.floor(40/div);j++) {
                     let radian = p.radians(j*div);
@@ -96,8 +97,13 @@ window.registerP5Sketch((p) => {
                     x = cx+r*p.sin(radian+p.PI/2.5);
                     y = cy+r*p.cos(radian+p.PI/2.5);
                     p.push()
-                        p. fill("#4f7dab");
-                        p.ellipse(x, y, 1.5);
+                        p.noStroke();
+                        if(isRecording) {
+                            p. fill("#FFB240");
+                        } else {
+                            p. fill("#333333");
+                        }
+                        p.ellipse(x, y, 4);
                     p.pop()
                 }
             }
@@ -110,13 +116,13 @@ window.registerP5Sketch((p) => {
             p.text(textString, 0, p.height-3);
         p.pop();
         p.imageMode(p.CORNER);
-        p.image(mouth, cx-75, p.height/3, 100, mouth.height*(100)/mouth.width);
+        p.image(mouth, cx-75, p.height/3-25, 100, mouth.height*(100)/mouth.width);
         p.push()
             p.textSize(15);
             p.textStyle(p.BOLD)
-            if(freq) {
+            /*if(freq) {
                 p. text("Frequency: " + freq.toFixed(2) + "Hz", 0, p.height-30);
-            }
+            }*/
         p.pop()
         
         
