317f315b47f5caccf17ff06c33865f307e783b94
Elias Jarzombek <ejarz25@gmail.com>
2021-08-26 10:37:05 -0400

    add pdf build and update fonts

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,25 @@
 console.log('MAIN.JS loaded');
 
+function isAnyPartOfElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  // DOMRect { x: 8, y: 8, width: 100, height: 100, top: 8, right: 108, bottom: 108, left: 8 }
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  // http://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
+  const vertInView = rect.top <= windowHeight && rect.top + rect.height >= 0;
+  const horInView = rect.left <= windowWidth && rect.left + rect.width >= 0;
+
+  return vertInView && horInView;
+}
+
 /* UI Code for handling Menu open/close and highlighting of current page section */
 document.addEventListener('DOMContentLoaded', () => {
   const MENU_CLOSED_CLASSNAME = 'menu-is-closed';
   let chapterSection;
   let sections;
+  let sketches;
 
   const debounce = (callback, wait) => {
     let timeoutId = null;
@@ -35,6 +50,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
   chapterSection = document.querySelector('section[data-type="chapter"]');
   sections = document.querySelectorAll('section section');
+  sketches = document.querySelectorAll('.p5-figure');
+  console.log('SKETCHES', sketches);
 
   /* Read from local storage */
   /* TODO: can we do this before the first page render? */
@@ -58,7 +75,7 @@ document.addEventListener('DOMContentLoaded', () => {
   document.addEventListener(
     'scroll',
     debounce(() => {
-      let newActiveSectionId = null;
+      let newActiveSectionId;
       for (let i = sections.length - 1; i >= 0; i--) {
         const section = sections[i];
         const { top, bottom } = section.getBoundingClientRect();
@@ -79,6 +96,20 @@ document.addEventListener('DOMContentLoaded', () => {
           location.hash = `#${activeSectionId}`;
         }
         updateActiveMenuLink();
+
+        const sectionSketches = [...sketches].filter(
+          isAnyPartOfElementInViewport
+        );
+
+        console.log('section sketches', sectionSketches);
+
+        if (P5SketchManager.loopOnlyOnScreenSketches) {
+          P5SketchManager.pauseAllP5Sketches();
+
+          sectionSketches.forEach((e) => {
+            P5SketchManager.resumeP5Sketch(e.id);
+          });
+        }
       }
     }),
     200
